f36c5434042a6cc86c92af87af1c8aba8829f807
jahidulmomin <jahidul.momin@brilliant.com.bd>
2020-04-16 16:03:29 +0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <Header/>
+    <Wallet/>
+  </div>
+</template>
+
+<script>
+import Wallet from './components/Wallet.vue'
+import Header from './components/Header.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Wallet,
+    Header
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+
+hr {
+    max-width: 100%;
+}
+
+input[type=submit]{ 
+  border-radius: 4px;
+  font-weight: bold;
+  min-width: 140px;
+  min-height: 50px;
+  background-color: #fff;
+}
+
+input[type=submit]:hover{ 
+  color: #fff;
+  background-color: #2c3e50;
+  cursor: pointer;
+}
+
+input[type=submit]:focus{ 
+  outline: 0;
+}
+
+input[type=text]{ 
+  border-radius: 4px;
+  min-width: 360px;
+  min-height: 25px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template> 
+    <div id="header">
+        <div id="wrapper">
+        <h1> Ethereum Wallet </h1>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+
+</script>
+
+<style scoped>
+#logo { 
+    width: 62.5px;
+    height: 62.5px;
+}
+
+h1 {
+    padding-top: 10px;
+}
+
+#wrapper { 
+    display: flex;
+}
+
+</style>
